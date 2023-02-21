c33050932a6e4222ab6ff6badb0fb3c3237175bb
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2023-02-21 11:41:58 -0500

    added to-dos for twitch-auth

--- twitch-auth.php
+++ twitch-auth.php
@@ -1,2 +1,14 @@
 <?php
-    
+
+    function error(){
+        echo "Something went wrong, try again :(<br>";
+        echo "<a href='index.php'>Return</a>";
+        die();
+    }
+
+    $code = isset($_GET['code']) ? $_GET['code'] : error();
+
+    // TO-DOS
+    // POST -> https://id.twitch.tv/oauth2/token -> get user access token and refresh token
+    // GET ->  https://api.twitch.tv/helix/users -> get user information (channel name, id, etc)
+    // Save to the database all the info we need it
