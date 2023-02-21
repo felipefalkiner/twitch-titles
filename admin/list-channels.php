05a659c259ec2b1128bb044a398518ed6d3bd45a
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2023-02-21 14:59:03 -0500

    started admin modules with list-channels

---
+++ admin/list-channels.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Twitch Titles - Admin - List Channels</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php
+        require_once 'functions.php';
+        require_once 'database.php';
+
+        $connect = Database::createConnection();
+        $usersIds = returnChannelList();
+        $clientId = TWITCH_CLIENT_ID;
+
+        $token = returnServerToken();
+
+        $url = "https://api.twitch.tv/helix/streams?";
+
+        // this will not work for more than 100 channels
+        foreach($usersIds as $userId) {
+            $id = $userId["user_id"];
+            if($id != "")
+                $url = $url."user_id=$id&";
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Client-ID: $clientId","Authorization: Bearer $token"));
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $result = curl_exec($ch);
+        $array = json_decode($result, true);
+
+        // var_dump($array);
+
+        if(count($array['data']) == 0) {
+            echo "No channels live!";
+            die();
+        }
+
+        foreach($array['data'] as $user) {
+            $userId = $user['user_id'];
+            $apiTitle = $user['title'];
+            $username = $user['user_name'];
+
+            compareTitles($userId, $apiTitle);
+        }
+
+    ?>
+</body>
+</html>
